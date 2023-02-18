4e2da2dde4ab1ad040efc407b7d9812ecb575871
Marksu-u <Marksu-u@users.noreply.github.com>
2023-02-18 12:45:39 +0100

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,29 @@
+// imports
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const errorHandler = require('./middlewares/errorHandler');
+const apiRouter = require('./routes');
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`)
+    .then(() => {
+        console.log("Successfully connect to database")
+    }).catch(err => console.log(err))
+
+app.use("/api/v1", apiRouter);
+app.use(errorHandler);
+
+app.listen(process.env.PORT, function () {
+    console.log("server launch");
+});
