b296d26a1d9363462b35f02e74b7727a46ca49c4
Marksu-u <72389013+Marksu-u@users.noreply.github.com>
2023-02-24 14:55:38 +0100

    test api

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-// imports
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -11,19 +10,25 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }));
+  
+
 app.use(bodyParser.json());
-
-mongoose.set('strictQuery', false);
-
-mongoose.connect(`mongodb+srv://airbnb:${process.env.MONGODB_PASSWORD}@airbnbclusterjs.szpgtab.mongodb.net/?retryWrites=true&w=majority`)
-    .then(() => {
-        console.log("Successfully connect to database")
-    }).catch(err => console.log(err))
 
 app.use("/api/v1", apiRouter);
 app.use(errorHandler);
 
+mongoose.set('strictQuery', false);
+
+mongoose.connect(`mongodb+srv://airbnb:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`)
+    .then(() => {
+        console.log("Successfully connect to database")
+    }).catch(err => console.log(err))
+
 app.listen(process.env.PORT, function () {
-    console.log("server launched");
+    console.log("server launched on port " + process.env.PORT);
 });
